8310af26c1960b7cc2df18224b25a67ed11d8b1d
NaDoFe <nathanreis792@gmail.com>
2025-05-02 16:05:09 -0300

    02-05

---
+++ jogo.lua
@@ -0,0 +1,225 @@
+local jogo = {}
+
+local nave = {}
+local meteoros = {}
+local tiros = {}
+local meteoroImagem
+local meteoroGigante
+local larguraTela = love.graphics.getWidth()
+local alturaTela = love.graphics.getHeight()
+local velocidadeNave = 300
+local velocidadeTiro = 500
+local velocidadeMeteoro = 100
+local tempoUltimoMeteoro = 0
+local fontePontuacao = love.graphics.newFont(20)
+
+local score = 0
+local vidas = 5
+local jogoPausado = false
+local gameOver = false
+local fase = 1
+local destruicoesFase = 0
+local alvoFase = 20
+local meteoroFinal = nil
+
+function jogo.load()
+    nave.image = love.graphics.newImage("nave1.png")
+    meteoroImagem = love.graphics.newImage("meteoro.png")
+    meteoroGigante = love.graphics.newImage("meteoro.png") -- usa a mesma imagem, mas escala maior
+
+    nave.x = larguraTela / 2
+    nave.y = alturaTela / 1.2
+    nave.largura = nave.image:getWidth() * 0.2
+    nave.altura = nave.image:getHeight() * 0.5
+    nave.velocidade = velocidadeNave
+end
+
+function jogo.reiniciar()
+    meteoros = {}
+    tiros = {}
+    meteoroFinal = nil
+    score = 0
+    vidas = 5
+    gameOver = false
+    jogoPausado = false
+    fase = 1
+    destruicoesFase = 0
+    alvoFase = 20
+    tempoUltimoMeteoro = 0
+
+    nave.x = larguraTela / 2
+end
+
+function jogo.update(dt)
+    if gameOver or jogoPausado then return end
+
+    -- Movimentação da nave
+    if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
+        nave.x = nave.x - nave.velocidade * dt
+    elseif love.keyboard.isDown("right") or love.keyboard.isDown("d") then
+        nave.x = nave.x + nave.velocidade * dt
+    end
+
+    -- Limites da nave
+    if nave.x < 0 then
+        nave.x = 0
+    elseif nave.x + nave.largura > larguraTela then
+        nave.x = larguraTela - nave.largura
+    end
+
+    -- Tiros
+    for i = #tiros, 1, -1 do
+        tiros[i].y = tiros[i].y - velocidadeTiro * dt
+        if tiros[i].y < 0 then
+            table.remove(tiros, i)
+        end
+    end
+
+    -- Fase final
+    if fase == 3 and meteoroFinal then
+        meteoroFinal.y = meteoroFinal.y + 40 * dt
+        for i = #tiros, 1, -1 do
+            if checarColisao(meteoroFinal, tiros[i], 0.6) then
+                meteoroFinal.vida = meteoroFinal.vida - 1
+                table.remove(tiros, i)
+                if meteoroFinal.vida <= 0 then
+                    gameOver = true -- vitória
+                end
+            end
+        end
+        return
+    end
+
+    -- Meteoros
+    for i = #meteoros, 1, -1 do
+        local m = meteoros[i]
+        m.y = m.y + m.velocidade * dt
+
+        -- Colisão com tiro
+        for j = #tiros, 1, -1 do
+            if checarColisao(m, tiros[j], 0.3) then
+                table.remove(meteoros, i)
+                table.remove(tiros, j)
+                destruicoesFase = destruicoesFase + 1
+                score = score + 1
+                break
+            end
+        end
+
+        -- Passou da tela
+        if m and m.y > alturaTela then
+            table.remove(meteoros, i)
+            vidas = vidas - 1
+            if vidas <= 0 then
+                gameOver = true
+            end
+        end
+    end
+
+    -- Fases
+    if fase == 1 and destruicoesFase >= 20 then
+        fase = 2
+        alvoFase = 10
+        destruicoesFase = 0
+        velocidadeMeteoro = 150
+    elseif fase == 2 and destruicoesFase >= 10 then
+        fase = 3
+        meteoroFinal = {
+            x = larguraTela / 2 - 100,
+            y = -100,
+            vida = 20,
+            largura = meteoroGigante:getWidth() * 0.6,
+            altura = meteoroGigante:getHeight() * 0.6
+        }
+    end
+
+    -- Criar novos meteoros
+    tempoUltimoMeteoro = tempoUltimoMeteoro + dt
+    if tempoUltimoMeteoro > 1 and fase < 3 then
+        criarMeteoro()
+        tempoUltimoMeteoro = 0
+    end
+end
+
+function jogo.draw()
+    -- Nave
+    love.graphics.draw(nave.image, nave.x, nave.y, 0, 0.2, 0.2)
+
+    -- Meteoros
+    for _, meteoro in ipairs(meteoros) do
+        love.graphics.draw(meteoroImagem, meteoro.x, meteoro.y, 0, 0.3, 0.3)
+    end
+
+    -- Meteoro Final
+    if meteoroFinal then
+        love.graphics.draw(meteoroGigante, meteoroFinal.x, meteoroFinal.y, 0, 0.6, 0.6)
+        love.graphics.setColor(255, 0, 0)
+        love.graphics.printf("Vida do Meteoro Final: " .. meteoroFinal.vida, 0, 30, larguraTela, "center")
+        love.graphics.setColor(255, 255, 255)
+    end
+
+    -- Tiros
+    love.graphics.setColor(0, 255, 0)
+    for _, tiro in ipairs(tiros) do
+        love.graphics.rectangle("fill", tiro.x, tiro.y, 5, 10)
+    end
+    love.graphics.setColor(255, 255, 255)
+
+    -- Pontuação e vidas
+    love.graphics.setFont(fontePontuacao)
+    love.graphics.print("Meteoros destruídos: " .. score, 10, 10)
+    love.graphics.print("Vidas: " .. vidas, 100, 50)
+    love.graphics.print("Fase: " .. fase, 10, 40)
+
+    -- Pausa
+    if jogoPausado then
+        love.graphics.printf("JOGO PAUSADO\nPressione 'P' para continuar\n'M' para menu", 0, alturaTela / 2, larguraTela, "center")
+    end
+
+    -- Game Over
+    if gameOver then
+        love.graphics.setColor(255, 0, 0)
+        local msg = (fase == 3 and meteoroFinal and meteoroFinal.vida <= 0) and "VITÓRIA! TERRA SALVA!" or "TERRA DESTRUÍDA"
+        love.graphics.printf(msg, 0, alturaTela / 2, larguraTela, "center")
+        love.graphics.printf("Pressione 'R' para reiniciar", 0, alturaTela / 2 + 40, larguraTela, "center")
+        love.graphics.setColor(255, 255, 255)
+    end
+end
+
+function jogo.keypressed(key)
+    if key == "space" and not jogoPausado and not gameOver then
+        local tiro = {x = nave.x + nave.largura / 2 - 2.5, y = nave.y}
+        table.insert(tiros, tiro)
+    elseif key == "p" then
+        jogoPausado = not jogoPausado
+    elseif key == "m" and jogoPausado then
+        estado = "menu"
+        jogoPausado = false
+    elseif key == "r" and gameOver then
+        jogo.reiniciar()
+    elseif key == "backspace" then
+        estado = "menu"
+    end
+end
+
+function criarMeteoro()
+    local meteoro = {
+        x = math.random(0, larguraTela - meteoroImagem:getWidth() * 0.3),
+        y = -30,
+        velocidade = math.random(velocidadeMeteoro, velocidadeMeteoro + 50),
+    }
+    table.insert(meteoros, meteoro)
+end
+
+function checarColisao(obj1, obj2, escala)
+    escala = escala or 0.3
+    local obj1Right = obj1.x + meteoroImagem:getWidth() * escala
+    local obj1Bottom = obj1.y + meteoroImagem:getHeight() * escala
+    local obj2Right = obj2.x + 5
+    local obj2Bottom = obj2.y + 10
+
+    return obj2.x < obj1Right and obj2Right > obj1.x and
+           obj2.y < obj1Bottom and obj2Bottom > obj1.y
+end
+
+return jogo
--- main.lua
+++ main.lua
@@ -1,138 +1,27 @@
 -- Bibliotecas
 creditos = require("menus.creditos")
 controles = require("menus.controles")
+jogo = require("jogo")
 
 love.window.setMode(0, 0, { fullscreen = true, fullscreentype = "desktop" })
 
--- Configuração inicial
-local nave = {}
-local meteoros = {}
-local tiros = {}
-local explosoes = {} -- NOVO: Tabela de explosões
+-- Estado do jogo
+estado = "menu"
+opcaoSelecionada = 1
 
 -- Imagem do menu
-local imagemMenu 
-
--- Parâmetros do jogo
+local imagemMenu
 local larguraTela = love.graphics.getWidth()
-local alturaTela = love.graphics.getHeight()
-local velocidadeNave = 300
-local velocidadeTiro = 500
-local velocidadeMeteoro = 100
 
-local tempoUltimoMeteoro = 100
-local score = 0
-local jogoPausado = false
-local gameOver = false
-local vidas = 5
-
--- Estados do jogo
-local estado = "menu"
-local opcaoSelecionada = 1
-
--- Carregar fontes
-local fontePontuacao = love.graphics.newFont(20)
-
--- Função para inicializar o jogo
 function love.load()
-    nave.image = love.graphics.newImage("nave1.png")
-    meteoroImagem = love.graphics.newImage("meteoro.png")
     imagemMenu = love.graphics.newImage("menu.png")
-
-    nave.x = larguraTela / 2
-    nave.y = alturaTela / 1.2
-    nave.largura = nave.image:getWidth() * 0.2
-    nave.altura = nave.image:getHeight() * 0.5
-    nave.velocidade = velocidadeNave
-
     love.window.setTitle("METEOR SMASH")
-end
-
-function reiniciarJogo()
-    meteoros = {}
-    tiros = {}
-    explosoes = {}
-    score = 0
-    tempoUltimoMeteoro = 1
-    vidas = 5
-    gameOver = false
+    jogo.load() -- carregar os recursos do jogo
 end
 
 function love.update(dt)
-    if gameOver then return end
-
     if estado == "jogo" then
-        if jogoPausado then return end
-
-        -- Movimentação da nave
-        if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
-            nave.x = nave.x - nave.velocidade * dt
-        elseif love.keyboard.isDown("right") or love.keyboard.isDown("d") then
-            nave.x = nave.x + nave.velocidade * dt
-        end
-
-        if nave.x < 0 then
-            nave.x = 0
-        elseif nave.x + nave.largura > larguraTela then
-            nave.x = larguraTela - nave.largura
-        end
-
-        -- Atualizar tiros
-        for i = #tiros, 1, -1 do
-            tiros[i].y = tiros[i].y - velocidadeTiro * dt
-            if tiros[i].y < 0 then
-                table.remove(tiros, i)
-            end
-        end
-
-        -- Atualizar meteoros
-        for i = #meteoros, 1, -1 do
-            meteoros[i].y = meteoros[i].y + velocidadeMeteoro * dt
-
-            -- Verificar colisão com tiros
-            for j = #tiros, 1, -1 do
-                if checarColisao(meteoros[i], tiros[j]) then
-                    table.insert(explosoes, {
-                        x = meteoros[i].x,
-                        y = meteoros[i].y,
-                        tempo = 0.5
-                    })
-                    table.remove(meteoros, i)
-                    table.remove(tiros, j)
-                    score = score + 1
-                    break
-                end
-            end
-
-            -- Verificar se bateu no chão
-            if meteoros[i] and meteoros[i].y > alturaTela then
-                table.insert(explosoes, {
-                    x = meteoros[i].x,
-                    y = alturaTela - 30,
-                    tempo = 0.5
-                })
-                table.remove(meteoros, i)
-                vidas = vidas - 1
-                if vidas <= 0 then
-                    gameOver = true
-                end
-            end
-        end
-
-        -- Criar novos meteoros
-        tempoUltimoMeteoro = tempoUltimoMeteoro + dt
-        if tempoUltimoMeteoro > 1 then
-            criarMeteoro()
-            tempoUltimoMeteoro = 0
-        end
-
-        -- Atualizar explosões
-        for i = #explosoes, 1, -1 do
-            explosoes[i].tempo = explosoes[i].tempo - dt
-            if explosoes[i].tempo <= 0 then
-                table.remove(explosoes, i)
-            end
-        end
+        jogo.update(dt)
     end
 end
 
@@ -140,7 +29,7 @@ function love.draw()
     if estado == "menu" then
         desenharMenu()
     elseif estado == "jogo" then
-        desenharJogo()
+        jogo.draw()
     elseif estado == "controles" then
         controles.desenharControles()
     elseif estado == "creditos" then
@@ -165,65 +54,6 @@ function desenharMenu()
     end
 end
 
-function desenharJogo()
-    love.graphics.draw(nave.image, nave.x, nave.y, 0, 0.2, 0.2)
-
-    for _, meteoro in ipairs(meteoros) do
-        love.graphics.draw(meteoroImagem, meteoro.x, meteoro.y, 0, 0.3, 0.3)
-    end
-
-    for _, explosao in ipairs(explosoes) do
-        love.graphics.setColor(255, 165, 0)
-        love.graphics.circle("fill", explosao.x + 15, explosao.y, 20)
-    end
-    love.graphics.setColor(255, 255, 255)
-
-    love.graphics.setColor(0, 255, 0)
-    for _, tiro in ipairs(tiros) do
-        love.graphics.rectangle("fill", tiro.x, tiro.y, 5, 10)
-    end
-    love.graphics.setColor(255, 255, 255)
-
-    love.graphics.setFont(fontePontuacao)
-    love.graphics.print("Meteoros destruídos: " .. score, 10, 10)
-    love.graphics.print("Vidas: " .. vidas, 100, 50)
-
-    if jogoPausado then
-        love.graphics.printf("JOGO PAUSADO\nPressione 'P' para continuar\nPressione 'M' para voltar ao menu", 0, alturaTela / 2, larguraTela, "center")
-    end
-
-    if gameOver then
-        love.graphics.setColor(255, 0, 0)
-        love.graphics.printf("TERRA DESTRUÍDA", 0, alturaTela / 2, larguraTela, "center")
-        love.graphics.printf("Pressione 'R' para reiniciar", 0, alturaTela / 2 + 40, larguraTela, "center")
-        love.graphics.setColor(255, 255, 255)
-    end
-end
-
-function criarMeteoro()
-    local meteoro = {
-        x = math.random(0, larguraTela - meteoroImagem:getWidth() * 0.3),
-        y = -30,
-        velocidade = math.random(100, 200),
-    }
-    table.insert(meteoros, meteoro)
-end
-
-function checarColisao(meteoro, tiro)
-    local escala = 0.3
-    local meteoroLeft = meteoro.x
-    local meteoroRight = meteoro.x + meteoroImagem:getWidth() * escala
-    local meteoroTop = meteoro.y
-    local meteoroBottom = meteoro.y + meteoroImagem:getHeight() * escala
-
-    local tiroLeft = tiro.x
-    local tiroRight = tiro.x + 5
-    local tiroTop = tiro.y
-    local tiroBottom = tiro.y + 10
-
-    return tiroRight > meteoroLeft and tiroLeft < meteoroRight and tiroBottom > meteoroTop and tiroTop < meteoroBottom
-end
-
 function love.keypressed(key)
     if estado == "menu" then
         if key == "down" then
@@ -233,6 +63,7 @@ function love.keypressed(key)
         elseif key == "return" then
             if opcaoSelecionada == 1 then
                 estado = "jogo"
+                jogo.reiniciar()
             elseif opcaoSelecionada == 2 then
                 estado = "controles"
             elseif opcaoSelecionada == 3 then
@@ -240,27 +71,12 @@ function love.keypressed(key)
             elseif opcaoSelecionada == 4 then
                 love.event.quit()
             end
-        elseif key == "backspace" then
-            estado = "menu"
         end
     elseif estado == "controles" or estado == "creditos" then
         if key == "backspace" then
             estado = "menu"
         end
     elseif estado == "jogo" then
-        if key == "space" and not jogoPausado then
-            local tiro = {x = nave.x + nave.largura / 2 - 2.5, y = nave.y}
-            table.insert(tiros, tiro)
-        elseif key == "p" then
-            jogoPausado = not jogoPausado
-        elseif key == "m" and jogoPausado then
-            estado = "menu"
-            jogoPausado = false
-        elseif key == "r" and gameOver then
-            reiniciarJogo()
-            estado = "jogo"
-        elseif key == "backspace" then
-            estado = "menu"
-        end
+        jogo.keypressed(key)
     end
 end
