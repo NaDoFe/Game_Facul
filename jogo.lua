67436da1b8da96b36a75b658283b7d7012d9de2d
NaDoFe <nathanreis792@gmail.com>
2025-05-27 21:47:00 -0300

    27-05

--- jogo.lua
+++ jogo.lua
@@ -5,21 +5,23 @@ local meteoros = {}
 local tiros = {}
 local meteoroImagem
 local meteoroGigante
+local imagemBala
+local imagemFase1
+local imagemFase2
 
--- Dimensões da tela
 local larguraTela = 1350
 local alturaTela = 720
 love.window.setMode(larguraTela, alturaTela)
 
--- Configurações
 local velocidadeNave = 300
 local velocidadeTiro = 500
 local velocidadeMeteoro = 100
 local tempoUltimoMeteoro = 0
 
-local fontePontuacao = love.graphics.newFont("fonts/PressStart2P-Regular.ttf", 10)
 
--- Estado do jogo
+local fontePontuacao = love.graphics.newFont("fonts/PressStart2P-Regular.ttf", 20)
+love.graphics.setFont(fontePontuacao)
+
 local score = 0
 local vidas = 5
 local jogoPausado = false
@@ -29,15 +31,29 @@ local destruicoesFase = 0
 local alvoFase = 20
 local meteoroFinal = nil
 
--- Transição de fase
 local emTransicaoDeFase = false
 local tempoTransicao = 0
 local duracaoTransicao = 3
+local alphaTransicao = 0
+local transicaoTextoY = alturaTela
+local textoTransicao = ""
+local mostrarImagemFase2 = false
+
+local navesDisponiveis = {
+    love.graphics.newImage("assets/nave/nave1.png"),
+    love.graphics.newImage("assets/nave/nave2.png"),
+    love.graphics.newImage("assets/nave/nave3.png")
+}
+local naveSelecionada = 1
 
 function jogo.load()
-    nave.image = love.graphics.newImage("nave1.png")
-    meteoroImagem = love.graphics.newImage("meteoro.png")
-    meteoroGigante = love.graphics.newImage("meteoro.png") -- usa a mesma imagem, mas escalada
+    nave.image = navesDisponiveis[naveSelecionada]
+    meteoroImagem = love.graphics.newImage("assets/meteoro/meteoro.png")
+    meteoroGigante = love.graphics.newImage("assets/meteoro/meteoro2.png")
+    imagemBala = love.graphics.newImage("assets/nave/bala.png")
+    imagemFase1 = love.graphics.newImage("assets/mapa/mapa1.png")
+    imagemFase2 = love.graphics.newImage("assets/mapa/mapa1.png")
+    imagemFase3 = love.graphics.newImage("assets/mapa/mapa1.png")
 
     nave.x = larguraTela / 2
     nave.y = alturaTela * 0.85
@@ -60,7 +76,15 @@ function jogo.reiniciar()
     tempoUltimoMeteoro = 0
     emTransicaoDeFase = false
     tempoTransicao = 0
+    alphaTransicao = 0
+    transicaoTextoY = alturaTela
+    textoTransicao = ""
+    mostrarImagemFase2 = false
     nave.x = larguraTela / 2
+
+    nave.image = navesDisponiveis[naveSelecionada]
+    nave.largura = nave.image:getWidth() * 0.2
+    nave.altura = nave.image:getHeight() * 2
 end
 
 function jogo.update(dt)
@@ -68,9 +92,16 @@ function jogo.update(dt)
 
     if emTransicaoDeFase then
         tempoTransicao = tempoTransicao + dt
+        alphaTransicao = math.sin((tempoTransicao / duracaoTransicao) * math.pi)
+        transicaoTextoY = alturaTela / 2 + math.sin(tempoTransicao * 3) * 10
+
         if tempoTransicao >= duracaoTransicao then
             emTransicaoDeFase = false
             tempoTransicao = 0
+            alphaTransicao = 0
+            transicaoTextoY = alturaTela
+            mostrarImagemFase2 = false
+
             if fase == 3 then
                 meteoroFinal = {
                     x = larguraTela / 2 - 100,
@@ -84,21 +115,18 @@ function jogo.update(dt)
         return
     end
 
-    -- Movimentação da nave
     if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
         nave.x = nave.x - nave.velocidade * dt
     elseif love.keyboard.isDown("right") or love.keyboard.isDown("d") then
         nave.x = nave.x + nave.velocidade * dt
     end
 
-    -- Limites da nave
     if nave.x < 0 then
         nave.x = 0
     elseif nave.x + nave.largura > larguraTela then
         nave.x = larguraTela - nave.largura
     end
 
-    -- Tiros
     for i = #tiros, 1, -1 do
         tiros[i].y = tiros[i].y - velocidadeTiro * dt
         if tiros[i].y < 0 then
@@ -106,12 +134,10 @@ function jogo.update(dt)
         end
     end
 
-    -- Meteoros
     for i = #meteoros, 1, -1 do
         local m = meteoros[i]
         m.y = m.y + m.velocidade * dt
 
-        -- Colisão com tiro
         for j = #tiros, 1, -1 do
             if checarColisao(m, tiros[j], 0.3) then
                 table.remove(meteoros, i)
@@ -122,7 +148,6 @@ function jogo.update(dt)
             end
         end
 
-        -- Passou da tela
         if m and m.y > alturaTela then
             table.remove(meteoros, i)
             vidas = vidas - 1
@@ -132,14 +157,12 @@ function jogo.update(dt)
         end
     end
 
-    -- Lógica de fases
     if fase == 1 and destruicoesFase >= 20 then
         iniciarTransicao(2, 10, 150)
     elseif fase == 2 and destruicoesFase >= 10 then
         iniciarTransicao(3)
     end
 
-    -- Fase final
     if fase == 3 and meteoroFinal then
         meteoroFinal.y = meteoroFinal.y + 40 * dt
         for i = #tiros, 1, -1 do
@@ -154,7 +177,6 @@ function jogo.update(dt)
         return
     end
 
-    -- Criar meteoros
     tempoUltimoMeteoro = tempoUltimoMeteoro + dt
     if tempoUltimoMeteoro > 1 and fase < 3 then
         criarMeteoro()
@@ -163,6 +185,25 @@ function jogo.update(dt)
 end
 
 function jogo.draw()
+    -- Desenhar fundo fase 1
+    if fase == 1 then
+        love.graphics.draw(imagemFase1, 0, 0, 0,
+            larguraTela / imagemFase1:getWidth(),
+            alturaTela / imagemFase1:getHeight()
+        )
+            -- Desenhar fundo fase 2   
+    elseif fase == 2 then
+        love.graphics.draw(imagemFase2, 0, 0, 0,
+            larguraTela / imagemFase2:getWidth(),
+            alturaTela / imagemFase2:getHeight()
+        )
+    else
+        love.graphics.draw(imagemFase3, 0, 0, 0,
+            larguraTela / imagemFase2:getWidth(),
+            alturaTela / imagemFase2:getHeight()
+        )
+    end
+
     love.graphics.draw(nave.image, nave.x, nave.y, 0, 0.2, 0.2)
 
     for _, meteoro in ipairs(meteoros) do
@@ -176,23 +217,29 @@ function jogo.draw()
         love.graphics.setColor(255, 255, 255)
     end
 
-    love.graphics.setColor(0, 255, 0)
     for _, tiro in ipairs(tiros) do
-        love.graphics.rectangle("fill", tiro.x, tiro.y, 5, 10)
+        love.graphics.draw(tiro.imagem, tiro.x, tiro.y, 0, 0.3, 0.3)
     end
-    love.graphics.setColor(255, 255, 255)
 
     love.graphics.setFont(fontePontuacao)
     love.graphics.print("Meteoros destruídos: " .. score, 10, 10)
     love.graphics.print("Fase: " .. fase, 10, 40)
-    love.graphics.print("Vidas: " .. vidas, 100, 50)
+    love.graphics.print("Vidas: " .. vidas, 10, 100)
+    love.graphics.print("Nave: " .. naveSelecionada, 10, 70)
 
     if jogoPausado then
         love.graphics.printf("JOGO PAUSADO\nPressione 'P' para continuar\n'M' para menu", 0, alturaTela / 2, larguraTela, "center")
     end
 
     if emTransicaoDeFase then
-        love.graphics.printf("Fase " .. fase .. " começando em " .. math.ceil(duracaoTransicao - tempoTransicao), 0, alturaTela / 2, larguraTela, "center")
+        local alpha = alphaTransicao * 255
+        love.graphics.setColor(0, 0, 0, alpha)
+        love.graphics.rectangle("fill", 0, 0, larguraTela, alturaTela)
+
+        love.graphics.setColor(255, 255, 255, alpha)
+        love.graphics.setFont(love.graphics.newFont(40))
+        love.graphics.printf(textoTransicao, 0, transicaoTextoY, larguraTela, "center")
+        love.graphics.setColor(255, 255, 255, 255)
     end
 
     if gameOver then
@@ -206,17 +253,38 @@ end
 
 function jogo.keypressed(key)
     if key == "space" and not jogoPausado and not gameOver and not emTransicaoDeFase then
-        local tiro = {x = nave.x + nave.largura / 2 - 2.5, y = nave.y}
+        local escalaBala = 0.3
+        local larguraBala = imagemBala:getWidth() * escalaBala
+        local alturaBala = imagemBala:getHeight() * escalaBala
+
+        local tiro = {
+            imagem = imagemBala,
+            largura = larguraBala,
+            altura = alturaBala,
+            y = nave.y - alturaBala
+        }
+
+        tiro.x = nave.x + nave.largura / 2 - larguraBala / 2
         table.insert(tiros, tiro)
     elseif key == "p" then
         jogoPausado = not jogoPausado
     elseif key == "m" and jogoPausado then
         estado = "menu"
         jogoPausado = false
+        gameOver = false
     elseif key == "r" and gameOver then
         jogo.reiniciar()
     elseif key == "backspace" then
         estado = "menu"
+    elseif key == "1" then
+        naveSelecionada = 1
+        nave.image = navesDisponiveis[naveSelecionada]
+    elseif key == "2" then
+        naveSelecionada = 2
+        nave.image = navesDisponiveis[naveSelecionada]
+    elseif key == "3" then
+        naveSelecionada = 3
+        nave.image = navesDisponiveis[naveSelecionada]
     end
 end
 
@@ -233,8 +301,8 @@ function checarColisao(obj1, obj2, escala)
     escala = escala or 0.3
     local obj1Right = obj1.x + meteoroImagem:getWidth() * escala
     local obj1Bottom = obj1.y + meteoroImagem:getHeight() * escala
-    local obj2Right = obj2.x + 5
-    local obj2Bottom = obj2.y + 10
+    local obj2Right = obj2.x + obj2.largura
+    local obj2Bottom = obj2.y + obj2.altura
 
     return obj2.x < obj1Right and obj2Right > obj1.x and
            obj2.y < obj1Bottom and obj2Bottom > obj1.y
@@ -248,6 +316,12 @@ function iniciarTransicao(novaFase, novoAlvo, novaVelocidade)
     destruicoesFase = 0
     meteoros = {}
     tiros = {}
+    tempoTransicao = 0
+    alphaTransicao = 0
+    transicaoTextoY = alturaTela
+    textoTransicao = "Fase " .. fase .. " começando..."
+    mostrarImagemFase2 = (fase == 2)
 end
+
 
 return jogo
--- main.lua
+++ main.lua
@@ -3,24 +3,84 @@ creditos = require("menus.creditos")
 controles = require("menus.controles")
 jogo = require("jogo")
 
--- Dimensões da tela
-larguraTela, alturaTela = love.window.getDesktopDimensions()
+
+estado = "menu"
+local larguraTela = 1350
+local alturaTela = 720
 love.window.setMode(larguraTela, alturaTela, {fullscreen = true})
 
--- Estado do jogo
-estado = "menu"
-opcaoSelecionada = 1
+love.window.setTitle("METEOR SMASH")
 
-local fonte = love.graphics.newFont("fonts/PressStart2P-Regular.ttf", 10)
+local jogo = require "jogo"
 
--- Imagem do menu
-local imagemMenu
+local fontePontuacao = love.graphics.newFont("fonts/PressStart2P-Regular.ttf", 20)
+love.graphics.setFont(fontePontuacao)
+
+-- Imagens
+local imagemMenu = love.graphics.newImage("assets/menu.png")
+
+-- Botões normais
+local botaoJogar = love.graphics.newImage("assets/menu/jogar.png")
+local botaoControles = love.graphics.newImage("assets/menu/controles.png")
+local botaoCreditos = love.graphics.newImage("assets/menu/creditos.png")
+local botaoSair = love.graphics.newImage("assets/menu/sair.png")
+
+-- Botões com hover
+local botaoJogarHover = love.graphics.newImage("assets/menu/jogar_hover.png")
+local botaoControlesHover = love.graphics.newImage("assets/menu/controles_hover.png")
+local botaoCreditosHover = love.graphics.newImage("assets/menu/creditos_hover.png")
+local botaoSairHover = love.graphics.newImage("assets/menu/sair_hover.png")
+
+-- Largura desejada para os botões
+local larguraDesejada = 170
+
+-- Lista de botões
+local botoes = {
+    {
+        nome = "jogar",
+        y = 350,
+        normal = botaoJogar,
+        hover = botaoJogarHover,
+        acao = function()
+            estado = "jogo"
+            jogo.reiniciar()
+        end
+    },
+    {
+        nome = "controles",
+        y = 430,
+        normal = botaoControles,
+        hover = botaoControlesHover,
+        acao = function() estado = "controles" end
+    },
+    {
+        nome = "creditos",
+        y = 510,
+        normal = botaoCreditos,
+        hover = botaoCreditosHover,
+        acao = function() estado = "creditos" end
+    },
+    {
+        nome = "sair",
+        y = 590,
+        normal = botaoSair,
+        hover = botaoSairHover,
+        acao = function() love.event.quit() end
+    }
+}
+
+-- Ajusta escala e posição X dos botões para centralizar e redimensionar
+for _, botao in ipairs(botoes) do
+    local larguraOriginal = botao.normal:getWidth()
+    botao.escala = larguraDesejada / larguraOriginal
+    local larguraRedimensionada = botao.normal:getWidth() * botao.escala
+    botao.x = (larguraTela - larguraRedimensionada) / 2
+end
+
+local botaoSelecionado = 1 -- Para navegação com teclado
 
 function love.load()
-    love.graphics.setFont(fonte)
-    imagemMenu = love.graphics.newImage("menu.png")
-    love.window.setTitle("METEOR SMASH")
-    jogo.load() -- carregar os recursos do jogo
+    jogo.load()
 end
 
 function love.update(dt)
@@ -31,69 +91,66 @@ end
 
 function love.draw()
     if estado == "menu" then
-        desenharMenu()
+        love.graphics.draw(imagemMenu, 0, 0, 0,
+            larguraTela / imagemMenu:getWidth(),
+            alturaTela / imagemMenu:getHeight()
+        )
+
+        for i, botao in ipairs(botoes) do
+            local imagem = (i == botaoSelecionado) and botao.hover or botao.normal
+            love.graphics.draw(imagem, botao.x, botao.y, 0, botao.escala, botao.escala)
+        end
+
     elseif estado == "jogo" then
         jogo.draw()
+
     elseif estado == "controles" then
         controles.desenharControles()
+
     elseif estado == "creditos" then
         creditos.desenharCreditos()
+
     end
 end
 
-function desenharMenu()
-    love.graphics.setFont(fonte)
-    love.graphics.setColor(255, 255, 255)
-
-    -- Escala proporcional para preencher a tela sem distorcer
-    local imgLarg = imagemMenu:getWidth()
-    local imgAlt = imagemMenu:getHeight()
-    local escala = math.min(larguraTela / imgLarg, alturaTela / imgAlt)
-
-    -- Centralizar a imagem
-    local offsetX = (larguraTela - imgLarg * escala) / 2
-    local offsetY = (alturaTela - imgAlt * escala) / 2
-    love.graphics.draw(imagemMenu, offsetX, offsetY, 0, escala, escala)
-
-    -- Título e opções
-    love.graphics.setColor(255, 255, 255)
-    love.graphics.printf("METEOR SMASH", 0, 100, larguraTela, "center")
-
-    local opcoes = {"Jogar", "Controles", "Créditos", "Sair"}
-    for i = 1, #opcoes do
-        if i == opcaoSelecionada then
-            love.graphics.setColor(100, 100, 0)
-            love.graphics.rectangle("line", 540, 192 + (i * 40), 200, 30)
-        else
-            love.graphics.setColor(255, 255, 255)
+function love.mousepressed(x, y, button)
+    if estado == "menu" and button == 1 then
+        for _, botao in ipairs(botoes) do
+            if estaSobre(botao, x, y) then
+                botao.acao()
+                break
+            end
         end
-        love.graphics.printf(opcoes[i], 0, 200 + (i * 40), larguraTela, "center")
     end
 end
 
 function love.keypressed(key)
     if estado == "menu" then
-        if key == "down" then
-            opcaoSelecionada = opcaoSelecionada % 4 + 1
-        elseif key == "up" then
-            opcaoSelecionada = (opcaoSelecionada - 2) % 4 + 1
-        elseif key == "return" then
-            if opcaoSelecionada == 1 then
-                estado = "jogo"
-                jogo.reiniciar()
-            elseif opcaoSelecionada == 2 then
-                estado = "controles"
-            elseif opcaoSelecionada == 3 then
-                estado = "creditos"
-            elseif opcaoSelecionada == 4 then
-                love.event.quit()
+        if key == "up" then
+            botaoSelecionado = botaoSelecionado - 1
+            if botaoSelecionado < 1 then
+                botaoSelecionado = #botoes
             end
-        end
-    elseif estado == "controles" or estado == "creditos" then
-        if key == "backspace" then
-            estado = "menu"
+        elseif key == "down" then
+            botaoSelecionado = botaoSelecionado + 1
+            if botaoSelecionado > #botoes then
+                botaoSelecionado = 1
+            end
+        elseif key == "return" or key == "kpenter" then
+            botoes[botaoSelecionado].acao()
+        elseif key == "escape" then
+            love.event.quit()
         end
     elseif estado == "jogo" then
         jogo.keypressed(key)
+    elseif key == "backspace" then
+        estado = "menu"
     end
+end
+
+function estaSobre(botao, mouseX, mouseY)
+    local larguraBotao = botao.normal:getWidth() * botao.escala
+    local alturaBotao = botao.normal:getHeight() * botao.escala
+    return mouseX >= botao.x and mouseX <= botao.x + larguraBotao and
+           mouseY >= botao.y and mouseY <= botao.y + alturaBotao
 end
